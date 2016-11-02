126f4b1552c540fa94d4b333ca7af98761f27a9b
wangyi0559 <969392953@qq.com>
2016-11-02 16:41:48 +0800

    Signed-off-by: wangyi0559 <969392953@qq.com>

--- src/main/java/QianBao/BillTradingPlatform/Service/RestService.java
+++ src/main/java/QianBao/BillTradingPlatform/Service/RestService.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import QianBao.BillTradingPlatform.Entity.Account;
+import QianBao.BillTradingPlatform.Entity.Bill;
 import QianBao.BillTradingPlatform.Entity.Credit;
 import QianBao.BillTradingPlatform.Entity.Guaranteed;
 import QianBao.BillTradingPlatform.Entity.Payment;
@@ -21,7 +22,7 @@ import QianBao.BillTradingPlatform.Entity.testEntity;
 
 /**
  * 
- * @author ñãÔÂ
+ * @author ï¿½ï¿½ï¿½ï¿½
  * @create 2016.11.1
  */
 @Service
@@ -81,7 +82,7 @@ public class RestService {
 			return 0;
 		}
 
-	};
+	}
 
 	public long initCredit(Credit Credit) {
 		try {
@@ -131,6 +132,19 @@ public class RestService {
 		} catch (Exception e) {
 			return false;
 		}
+	}
+	
+	public long initBill(Bill bill ) {
+		try {
+			jdbcTemplate
+					.update("insert into tb_bill(Bill_UserID,Bill_Denomination,Bill_price,bill_AcceptingBank,bill_State) values(?,?,?,?,?)",
+							new Object[] { bill.getBill_UserID(),bill.getBill_Denomination(),bill.getBill_price(),bill.getBill_AcceptingBank(),bill.getBill_State() });
+			return getSequence("tb_bill");
+		} catch (Exception e) {
+			System.out.println(e);
+			return 0;
+		}
+
 	}
 
 
